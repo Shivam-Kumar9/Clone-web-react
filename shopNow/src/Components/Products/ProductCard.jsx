e11766dc1254496a6a3bcce7cc2a3262e855ed4c
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-18 23:29:13 +0530

    fix: Adjust formatting in ProductCard component for consistent tag display

--- shopNow/src/Components/Products/ProductCard.jsx
+++ shopNow/src/Components/Products/ProductCard.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 function ProductCard({ key, title, price, discountPercentage, rating, tags, images, thumbnail }) {
   return (
     <div className='product_card' key={key}>
-      <img src={thumbnail} alt={title} />
+      <img src={thumbnail} alt={title}/>
       <h2>{title}</h2>
       <p>Price: ${price}</p>
       <p>Discount: {discountPercentage}%</p>
       <p>Rating: {rating}</p>
       <div>
-        Tags: {tags.map(tag => <span key={tag}>{tag}</span>)}
+        Tags: {tags.map(tag => <span key={tag}>{tag} </span>)}
       </div>
     </div>
   )
